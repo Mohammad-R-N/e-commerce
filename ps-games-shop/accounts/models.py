af97139c89a197b916bd0b5a7a5d6883e3126d9e
mohammad-r-n <mohammad.ca@proton.me>
2023-10-05 08:59:43 -0400

    chore: account model

--- ps-games-shop/accounts/models.py
+++ ps-games-shop/accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Account(AbstractUser,PermissionsMixin):
+    
+    username = models.CharField(max_length=100,verbose_name=_("User Name"))
+    email = models.EmailField(unique=True,verbose_name=_("Email"))
+    password = models.CharField(max_length=18,verbose_name=_("Password"))
+    date_joined = models.DateTimeField(verbose_name=_("Joined Date"), auto_now_add=True, editable=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    first_name = None
+    last_name = None
+    username = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self) -> str:
+        return self.username
